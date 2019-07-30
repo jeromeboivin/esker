1d706253026a7bd55d4d0e5e5d7ad658162da080
Jérôme Boivin <boivin.jerome@gmail.com>
2019-07-30 14:10:14 +0200

    Added SuperMarioBros NES game

--- Processes/build.bat
+++ Processes/build.bat
@@ -1,2 +1,3 @@
 c:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Passwords.proj
 c:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe MarkdownEditor.proj
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe SuperMarioBros.proj
